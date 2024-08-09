1adb923c20428568f0a96eac159d7b3d30ac6e71
Striver-3110 <21csjay067@ldce.ac.in>
2024-08-09 12:57:42 +0530

    validations & toast added to register component

--- public/chat-app/src/pages/Register.jsx
+++ public/chat-app/src/pages/Register.jsx
@@ -2,19 +2,54 @@ import styled from 'styled-components'
 import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
 
 function Register () {
+  const [value, setValue] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
 
-    const [value, setValue] = useState({
-        name:'',
-        email:'',
-        password:'',
-        confirmPassword:'',
-    })
+  const handleSubmit = e => {
+    e.preventDefault()
+    handleValidation()
+  }
 
-  const handleSubmit = () => {}
-  const onChange = (e) =>{
-    setValue({...value, [e.target.name]:e.target.value});
+  const toastOptions = {
+    position: 'bottom-left',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    theme: 'light'
+    // transition: Bounce,
+  }
+  const handleValidation = () => {
+    const { name, email, password, confirmPassword } = value
+    if (
+      name === '' ||
+      email === '' ||
+      password === '' ||
+      confirmPassword === ''
+    ) {
+      toast.error('Please fill all the fields', toastOptions)
+    }
+
+    if (password !== confirmPassword) {
+      toast.error('password and confirm password must be same!', toastOptions)
+    }
+    if (name.length < 3) {
+      toast.error('name must be at least 3 characters', toastOptions)
+    }
+    if(password.length < 8) {
+        toast.error('password must be at least 8 characters', toastOptions)
+    }
+  }
+  const onChange = e => {
+    setValue({ ...value, [e.target.name]: e.target.value })
   }
   return (
     <>
@@ -24,9 +59,24 @@ function Register () {
             <img src={logo} alt='logo' />
             <h1>snappy</h1>
           </div>
-          <input onChange={onChange} type='text' placeholder='Username' name='name' />
-          <input onChange={onChange} type='email' placeholder='Email' name='email' />
-          <input onChange={onChange} type='password' placeholder='Password' name='password' />
+          <input
+            onChange={onChange}
+            type='text'
+            placeholder='Username'
+            name='name'
+          />
+          <input
+            onChange={onChange}
+            type='email'
+            placeholder='Email'
+            name='email'
+          />
+          <input
+            onChange={onChange}
+            type='password'
+            placeholder='Password'
+            name='password'
+          />
           <input
             onChange={onChange}
             type='password'
@@ -40,6 +90,7 @@ function Register () {
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   )
 }
@@ -74,41 +125,41 @@ const FormContainer = styled.div`
     background: #00000076;
     border-radius: 2rem;
     padding: 3rem 5rem;
-    input{
-        background-color:transparent;
-        padding: 1rem;
-        border: 0.1rem solid #4e0eff;
-        border-radius: 0.4rem;
-        width: 100%;
-        font-size:1rem;
-        color: white;
-        &:focus{
-            border: 0.1rem solid #997af0;
-            outline: none;
-        }
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      width: 100%;
+      font-size: 1rem;
+      color: white;
+      &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
+      }
     }
-    button{
-        background-color: #997af0;
-        color: white;
-        padding: 1rem 2rem;
-        width: 100%;
-        text-transform: uppercase;
-        cursor: pointer;
-        border-radius: 0.4rem;
-        transition: 0.5s ease-in-out;
+    button {
+      background-color: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      width: 100%;
+      text-transform: uppercase;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      transition: 0.5s ease-in-out;
+      font-weight: bold;
+      &:hover {
+        background-color: #4e0eff;
+      }
+    }
+    span {
+      color: white;
+      text-transform: uppercase;
+      a {
+        color: #4e0eff;
+        text-decoration: none;
         font-weight: bold;
-        &:hover{
-            background-color: #4e0eff;
-        }
-    }
-    span{
-        color:white;
-        text-transform: uppercase;
-        a{
-            color: #4e0eff;
-            text-decoration: none;
-            font-weight: bold;
-        }
+      }
     }
   }
 `
